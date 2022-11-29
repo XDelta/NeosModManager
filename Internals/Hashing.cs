04127cf60a0d63701e5ec131004475670ea63c8e
Delta <delta@deltawolf.tk>
2022-11-28 19:42:04 -0800

    Moved Hashing out of install manager

---
+++ Internals/Hashing.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+namespace NeosModManager.Internals;
+
+public class Hashing {
+
+	public static string GenerateSHA256(string filepath) {
+		if (!File.Exists(filepath)) {
+			Log($"Unable to find file:'{filepath}'", Level.ERROR);
+			return "";
+		}
+		try {
+			using (var hasher = SHA256.Create()) {
+				using (var stream = File.OpenRead(filepath)) {
+					var hash = hasher.ComputeHash(stream);
+					return BitConverter.ToString(hash).Replace("-", "").ToLower();
+				}
+			}
+		} catch (Exception e) {
+			Log(e);
+			return "";
+		}
+	}
+}
